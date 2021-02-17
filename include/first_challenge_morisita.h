51b28aa2de5d52541184dd0c85f32cde3e2e929a
morisitakanta <morisitakanta@gmail.com>
2021-02-17 19:28:03 +0900

    test

--- include/first_challenge_morisita.h
+++ include/first_challenge_morisita.h
@@ -4,6 +4,8 @@
 #include <ros/ros.h>
 #include <vector>
 #include "roomba_500driver_meiji/RoombaCtrl.h"
+#include "nav_msgs/Odometry.h"
+#include "tf/tf.h"
 //#include "sensor_msgs/LaserScan.h"
 
 
@@ -16,13 +18,16 @@ class FirstChallenge
     private:
         //void laser_callback(const )
         void turn();
+        void odometry_callback(const nav_msgs::Odometry::ConstPtr &);
 
         int hz;
+        double r,p,y;
 
         ros::Publisher pub_roomba_ctrl;
-        //ros::Subscriver sub_;
+        ros::Subscriber sub_odometry;
         ros::NodeHandle nh;
         ros::NodeHandle private_nh;
+        nav_msgs::Odometry odometry;
 
 };
 #endif
