39af84b20b3a8c8c2a4d2379f1138441941dfabe
morisitakanta <morisitakanta@gmail.com>
2021-03-05 12:52:02 +0900

    first sccess

--- src/first_challenge_morisita.cpp
+++ src/first_challenge_morisita.cpp
@@ -4,6 +4,7 @@ FirstChallenge::FirstChallenge():private_nh("~")
     private_nh.param("hz",hz,{10});
     private_nh.param("turn_flag",turn_flag,{false});
     private_nh.param("runnnig_flag",runnnig_flag,{true});
+    private_nh.param("forward_mode",forward_mode,{0});
     sub_odometry=nh.subscribe("/roomba/odometry",100,&FirstChallenge::odometry_callback,this);
     sub_laser=nh.subscribe("/scan",100,&FirstChallenge::laser_callback,this);
     pub_roomba_ctrl=nh.advertise<roomba_500driver_meiji::RoombaCtrl>("/roomba/control",1);
@@ -35,8 +36,7 @@ void FirstChallenge::turn()
     if((turn_start_point-0.2)<yaw && yaw<turn_start_point){
         turn_flag=false;
         cmd_vel.cntl.angular.z=0;
-        odometry.pose.pose.position.x=0;
-        std::cout<<"position.x = "<<odometry.pose.pose.position.x<<std::endl;
+        forward_mode=1;
     }
 
     pub_roomba_ctrl.publish(cmd_vel);
@@ -52,11 +52,11 @@ void FirstChallenge::forward()
     tf::Matrix3x3(quat).getRPY(roll,pitch,yaw);
     turn_start_point=yaw;
 
-    if(odometry.pose.pose.position.x > 0.1){
+    if(odometry.pose.pose.position.x > 0.1 && forward_mode==0){
         turn_flag=true;
         cmd_vel.cntl.linear.x=0;
     }
-    else if(read()){
+    else if(read() && forward_mode==1){
         runnnig_flag=false;
         cmd_vel.cntl.linear.x=0;
         cmd_vel.cntl.angular.z=0;
@@ -67,9 +67,9 @@ void FirstChallenge::forward()
 int FirstChallenge::read()
 {
     if(laser.ranges.size()!=0){
-        if(laser.ranges[540] < 0.2){
-            std::cout<<"laser.ranges = "<<laser.ranges[540]<<std::endl;
+        if(laser.ranges[540] < 0.5){
             return true;
+            std::cout<<"laser.ranges = "<<laser.ranges[540]<<std::endl;
         }else return false;
     }else return false;
 }
